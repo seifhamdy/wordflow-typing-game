6f187128df43c94f4625144df9b02f8cc7888b15
seifhamdy <shsabry@uofcanada.edu.eg>
2023-06-13 22:49:27 +0300

    added color indicators

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,112 +1,108 @@
-import React, { useState, useEffect, ChangeEvent, useRef } from 'react';
-import Word from './Word';
+import React, { useState, useEffect, ChangeEvent, useRef } from 'react'
+import Word from './Word'
 
 const App: React.FC = () => {
-  const [input, setInput] = useState('');
-  const [words, setWords] = useState<string[]>([]);
-  const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [startTime, setStartTime] = useState<number | null>(null);
-  const [wordCount, setWordCount] = useState(0);
-  const [elapsedTime, setElapsedTime] = useState<number>(0);
-  const [wordsPerMinute, setWordsPerMinute] = useState(0);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [input, setInput] = useState('')
+  const [words, setWords] = useState<string[]>([])
+  const [currentWordIndex, setCurrentWordIndex] = useState(0)
+  const [score, setScore] = useState(0)
+  const [startTime, setStartTime] = useState<number | null>(null)
+  const [wordCount, setWordCount] = useState(0)
+  const [elapsedTime, setElapsedTime] = useState<number>(0)
+  const [wordsPerMinute, setWordsPerMinute] = useState(0)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const fetchRandomWords = async (count: number): Promise<string[]> => {
     try {
       const response = await fetch(
         `https://random-word-api.herokuapp.com/word?number=${count}`
-      );
-      const data = await response.json();
-      return data;
+      )
+      const data = await response.json()
+      return data
     } catch (error) {
-      console.log('Error fetching random words:', error);
-      return [];
+      console.log('Error fetching random words:', error)
+      return []
     }
-  };
+  }
 
-  const wordsPerLine = 7; // Set the number of words per line
+  const wordsPerLine = 7 // Set the number of words per line
 
   useEffect(() => {
     const fetchWords = async () => {
-      const fetchedWords = await fetchRandomWords(wordsPerLine);
-      setWords(fetchedWords);
-      setCurrentWordIndex(0);
-    };
-
-    fetchWords();
-  }, [wordsPerLine]);
-
-  const handleInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-
-    if (!startTime) {
-      setStartTime(Date.now());
+      const fetchedWords = await fetchRandomWords(wordsPerLine)
+      setWords(fetchedWords)
+      setCurrentWordIndex(0)
     }
 
-    const typedWord = e.target.value.trim();
-    const currentCorrectWord = words[currentWordIndex];
+    fetchWords()
+  }, [wordsPerLine])
+
+  const handleInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+
+    if (!startTime) {
+      setStartTime(Date.now())
+    }
+
+    const typedWord = e.target.value.trim()
+    const currentCorrectWord = words[currentWordIndex]
 
     if (typedWord === currentCorrectWord && e.target.value.endsWith(' ')) {
-      setInput('');
-      setScore((prevScore) => prevScore + 1);
+      setInput('')
+      setScore((prevScore) => prevScore + 1)
 
-      const newWords = [...words];
-      newWords.splice(currentWordIndex, 1);
-      const fetchedWord = (await fetchRandomWords(1))[0];
-      newWords.push(fetchedWord);
-      setWords(newWords);
+      const newWords = [...words]
+      newWords.splice(currentWordIndex, 1)
+      const fetchedWord = (await fetchRandomWords(1))[0]
+      newWords.push(fetchedWord)
+      setWords(newWords)
 
       setCurrentWordIndex((prevIndex) =>
         Math.min(prevIndex, newWords.length - 1)
-      );
-      setWordCount((prevCount) => prevCount + 1);
+      )
+      setWordCount((prevCount) => prevCount + 1)
     }
-  };
+  }
 
   useEffect(() => {
     if (startTime !== null) {
       const interval = setInterval(() => {
-        const elapsedTimeInSeconds = Math.floor(
-          (Date.now() - startTime) / 1000
-        );
-        setElapsedTime(elapsedTimeInSeconds);
-      }, 1000);
+        const elapsedTimeInSeconds = Math.floor((Date.now() - startTime) / 1000)
+        setElapsedTime(elapsedTimeInSeconds)
+      }, 1000)
 
-      return () => clearInterval(interval);
+      return () => clearInterval(interval)
     }
-  }, [startTime]);
+  }, [startTime])
 
   useEffect(() => {
     if (elapsedTime > 0 && wordCount > 0) {
-      const wordsPerMinuteValue = Math.round(
-        (wordCount / elapsedTime) * 60
-      );
-      setWordsPerMinute(wordsPerMinuteValue);
+      const wordsPerMinuteValue = Math.round((wordCount / elapsedTime) * 60)
+      setWordsPerMinute(wordsPerMinuteValue)
     } else {
-      setWordsPerMinute(0);
+      setWordsPerMinute(0)
     }
-  }, [elapsedTime, wordCount]);
+  }, [elapsedTime, wordCount])
 
   const handleDocumentMouseDown = (e: MouseEvent) => {
     if (inputRef.current && !inputRef.current.contains(e.target as Node)) {
-      e.preventDefault();
+      e.preventDefault()
     }
-  };
+  }
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleDocumentMouseDown);
+    document.addEventListener('mousedown', handleDocumentMouseDown)
 
     return () => {
-      document.removeEventListener('mousedown', handleDocumentMouseDown);
-    };
-  }, []);
+      document.removeEventListener('mousedown', handleDocumentMouseDown)
+    }
+  }, [])
 
   useEffect(() => {
     if (inputRef.current) {
-      inputRef.current.focus();
+      inputRef.current.focus()
     }
-  });
+  })
 
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center">
@@ -119,6 +115,7 @@ const App: React.FC = () => {
             <Word
               text={word}
               isActive={index === currentWordIndex}
+              typedInput={input}
               className={
                 index === currentWordIndex ? 'text-white' : 'text-gray-400'
               }
@@ -135,7 +132,7 @@ const App: React.FC = () => {
         className="absolute w-0 h-0 opacity-0"
       />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- client/src/Word.tsx
+++ client/src/Word.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
 
 interface WordProps {
-  text: string;
-  isActive: boolean;
-  className?: string;
+  text: string
+  isActive: boolean
+  typedInput: string
+  className?: string
 }
 
-const Word: React.FC<WordProps> = ({ text, isActive, className }) => {
-  return <span className={`${className} ${isActive ? 'text-white' : 'text-gray-400'}`}>{text}</span>;
+const Word: React.FC<WordProps> = ({
+  text,
+  isActive,
+  typedInput,
+  className,
+}) => {
+  const getLetterColor = (index: number) => {
+    if (!isActive) {
+      return 'text-gray-400'
+    }
+
+    const typedWord = typedInput.trim()
+    const currentCorrectWord = text.trim()
+    const typedLetters = typedWord.split('')
+    const correctLetters = currentCorrectWord.split('')
+
+    if (typedLetters.length <= index) {
+      return 'text-white'
+    } else if (typedLetters[index] === correctLetters[index]) {
+      return 'text-green-500'
+    } else {
+      return 'text-red-500'
+    }
+  }
+
+  return (
+    <span className={className}>
+      {text.split('').map((letter, index) => (
+        <span key={index} className={getLetterColor(index)}>
+          {letter}
+        </span>
+      ))}
+    </span>
+  )
 }
 
-export default Word;
+export default Word
