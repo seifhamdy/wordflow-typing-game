5236431794af596e084fced4ba4daa12d70296b0
seifhamdy <shsabry@uofcanada.edu.eg>
2023-06-14 00:30:56 +0300

    improved reactive spell checker

--- client/src/Word.tsx
+++ client/src/Word.tsx
@@ -1,46 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 interface WordProps {
-  text: string
-  isActive: boolean
-  typedInput: string
-  className?: string
+  text: string;
+  isActive: boolean;
+  typedInput: string;
+  className?: string;
 }
 
-const Word: React.FC<WordProps> = ({
-  text,
-  isActive,
-  typedInput,
-  className,
-}) => {
+const Word: React.FC<WordProps> = ({ text, isActive, typedInput, className }) => {
   const getLetterColor = (index: number) => {
     if (!isActive) {
-      return 'text-gray-400'
+      return 'text-gray-400';
     }
 
-    const typedWord = typedInput.trim()
-    const currentCorrectWord = text.trim()
-    const typedLetters = typedWord.split('')
-    const correctLetters = currentCorrectWord.split('')
+    const typedWord = typedInput.trim();
+    const currentCorrectWord = text.trim();
+    const typedLetters = typedWord.split('');
+    const correctLetters = currentCorrectWord.split('');
 
     if (typedLetters.length <= index) {
-      return 'text-white'
+      return 'text-white';
     } else if (typedLetters[index] === correctLetters[index]) {
-      return 'text-green-500'
+      return 'text-green-500';
     } else {
-      return 'text-red-500'
+      return 'text-red-500';
     }
-  }
+  };
+
+  const getDisplayedLetters = () => {
+    const typedWord = typedInput.trim();
+    const currentCorrectWord = text.trim();
+
+    if (!isActive) {
+      return text;
+    } else {
+      return typedWord.length >= currentCorrectWord.length ? typedWord : currentCorrectWord;
+    }
+  };
 
   return (
     <span className={className}>
-      {text.split('').map((letter, index) => (
-        <span key={index} className={getLetterColor(index)}>
+      {getDisplayedLetters().split('').map((letter, index) => (
+        <span
+          key={index}
+          className={getLetterColor(index)}
+        >
           {letter}
         </span>
       ))}
     </span>
-  )
-}
+  );
+};
 
-export default Word
+export default Word;
