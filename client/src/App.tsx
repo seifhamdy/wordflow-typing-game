5b9fa3ee6482f46ac583bcbd904a806a5cc6e07c
seifhamdy <shsabry@uofcanada.edu.eg>
2023-06-15 01:46:20 +0300

    adding extra letters to the end (green)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,111 +1,114 @@
-import React, { useEffect, useState, useRef } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import './index.css';
 
-const apiUrl = 'https://random-word-api.herokuapp.com/word?number=10' // API endpoint to fetch random words
-const wordWidth = 200 // Width of each word in pixels
-const fetchThreshold = 5 // Number of words remaining when new fetch is triggered
+const apiUrl = 'https://random-word-api.herokuapp.com/word?number=10'; // API endpoint to fetch random words
+const wordWidth = 200; // Width of each word in pixels
+const fetchThreshold = 5; // Number of words remaining when new fetch is triggered
 
 function App() {
-  const [words, setWords] = useState<string[]>([])
-  const [currentWordIndex, setCurrentWordIndex] = useState(0)
-  const [currentLetterIndex, setCurrentLetterIndex] = useState(0)
-  const [userInput, setUserInput] = useState('')
-  const [score, setScore] = useState(0)
-  const [maxWordsInLine, setMaxWordsInLine] = useState(0)
-  const wordRef = useRef<HTMLDivElement>(null)
+  const [words, setWords] = useState<string[]>([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+  const [userInput, setUserInput] = useState('');
+  const [score, setScore] = useState(0);
+  const [maxWordsInLine, setMaxWordsInLine] = useState(0);
+  const wordRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    fetchWords()
-    calculateMaxWordsInLine()
-    window.addEventListener('resize', calculateMaxWordsInLine)
+    fetchWords();
+    calculateMaxWordsInLine();
+    window.addEventListener('resize', calculateMaxWordsInLine);
     return () => {
-      window.removeEventListener('resize', calculateMaxWordsInLine)
-    }
-  }, [])
+      window.removeEventListener('resize', calculateMaxWordsInLine);
+    };
+  }, []);
 
   useEffect(() => {
-    setCurrentLetterIndex(0)
-    setUserInput('')
-  }, [currentWordIndex])
+    setCurrentLetterIndex(0);
+    setUserInput('');
+  }, [currentWordIndex]);
 
   useEffect(() => {
     const checkUserInput = () => {
-      const currentWord = words[currentWordIndex]
+      const currentWord = words[currentWordIndex];
       if (userInput.trim().toLowerCase() === currentWord.toLowerCase()) {
-        setCurrentWordIndex((prevIndex) => prevIndex + 1)
-        setScore((prevScore) => prevScore + 1)
-        setUserInput('')
-        setCurrentLetterIndex(0)
+        setCurrentWordIndex((prevIndex) => prevIndex + 1);
+        setScore((prevScore) => prevScore + 1);
+        setUserInput('');
+        setCurrentLetterIndex(0);
       }
-    }
+    };
+
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === ' ') {
-        event.preventDefault()
-        checkUserInput()
+        event.preventDefault();
+        checkUserInput();
       } else if (event.key === 'Backspace' && userInput.length > 0) {
-        setUserInput(userInput.slice(0, -1))
-        setCurrentLetterIndex(currentLetterIndex - 1)
+        setUserInput(userInput.slice(0, -1));
+        setCurrentLetterIndex(currentLetterIndex - 1);
       } else if (event.key.length === 1 && /^[a-zA-Z]+$/.test(event.key)) {
-        setUserInput(userInput + event.key)
-        setCurrentLetterIndex(currentLetterIndex + 1)
+        setUserInput(userInput + event.key);
+        setCurrentLetterIndex(currentLetterIndex + 1);
       }
-    }
+    };
 
-    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener('keydown', handleKeyDown);
     return () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [currentWordIndex, userInput, currentLetterIndex, words])
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentWordIndex, userInput, currentLetterIndex, words]);
 
   useEffect(() => {
     if (wordRef.current) {
-      wordRef.current.focus()
+      wordRef.current.focus();
     }
-  }, [currentWordIndex])
+  }, [currentWordIndex]);
 
   useEffect(() => {
     if (words.length - currentWordIndex <= fetchThreshold) {
-      fetchWords()
+      fetchWords();
     }
-  }, [currentWordIndex, words])
+  }, [currentWordIndex, words]);
 
   const fetchWords = async () => {
     try {
-      const response = await axios.get(apiUrl)
-      setWords((prevWords) => [...prevWords, ...response.data])
+      const response = await axios.get(apiUrl);
+      setWords((prevWords) => [...prevWords, ...response.data]);
     } catch (error) {
-      console.error('Error fetching words:', error)
+      console.error('Error fetching words:', error);
     }
-  }
+  };
 
   const calculateMaxWordsInLine = () => {
-    const availableWidth = window.innerWidth * 0.9 // Use 90% of the window width
-    const maxWords = Math.floor(availableWidth / wordWidth)
-    setMaxWordsInLine(maxWords)
-  }
+    const availableWidth = window.innerWidth * 0.9; // Use 90% of the window width
+    const maxWords = Math.floor(availableWidth / wordWidth);
+    setMaxWordsInLine(maxWords);
+  };
 
   const renderWordsInLine = () => {
-    const renderedWords: JSX.Element[] = []
+    const renderedWords: JSX.Element[] = [];
+  
     for (let i = 0; i < maxWordsInLine && i < words.length; i++) {
-      const word = words[i + currentWordIndex]
-      if (!word) break
-
-      const isCurrentWord = i === 0
-      // const isActive = isCurrentWord && currentLetterIndex < word.length;
-
-      const renderedWord: JSX.Element[] = []
-      for (let j = 0; j < word.length; j++) {
-        const letter = word[j]
-        const isCurrentLetter = isCurrentWord && j === currentLetterIndex
+      const word = words[i + currentWordIndex];
+      if (!word) break;
+  
+      const isCurrentWord = i === 0;
+      const extraLetters = isCurrentWord ? userInput.slice(word.length) : '';
+  
+      const renderedWord: JSX.Element[] = [];
+      for (let j = 0; j < word.length + extraLetters.length; j++) {
+        const letter = j < word.length ? word[j] : extraLetters[j - word.length];
+        const isCurrentLetter = isCurrentWord && j === currentLetterIndex;
         const isCorrectLetter =
           isCurrentWord &&
           j < userInput.length &&
-          letter.toLowerCase() === userInput[j].toLowerCase()
+          letter.toLowerCase() === userInput[j].toLowerCase();
         const isIncorrectLetter =
           isCurrentWord &&
           j < userInput.length &&
-          letter.toLowerCase() !== userInput[j].toLowerCase()
-
+          letter.toLowerCase() !== userInput[j].toLowerCase();
+  
         renderedWord.push(
           <span
             key={j}
@@ -127,9 +130,9 @@ function App() {
                 : letter}
             </span>
           </span>
-        )
+        );
       }
-
+  
       renderedWords.push(
         <span
           key={i}
@@ -139,11 +142,11 @@ function App() {
         >
           {renderedWord}
         </span>
-      )
+      );
     }
-
-    return renderedWords
-  }
+  
+    return renderedWords;
+  };
 
   return (
     <div
@@ -158,7 +161,7 @@ function App() {
         <div className="flex space-x-2">{renderWordsInLine()}</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
